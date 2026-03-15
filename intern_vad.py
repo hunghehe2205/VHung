5930b8a0ae4ced98522e6db824a372a916e8bd96
hungHCMUT <hung.nguyen2205@hcmut.edu.vn>
2026-03-15 15:36:57 +0700

    Initial commit

---
+++ intern_vad.py
@@ -0,0 +1,70 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import nn
+from VadCLIP.src.utils.layers import GraphConvolution, DistanceAdj
+from collections import OrderedDict
+
+class LayerNorm(nn.LayerNorm):
+
+    def forward(self, x: torch.Tensor):
+        orig_type = x.dtype
+        ret = super().forward(x.type(torch.float32))
+        return ret.type(orig_type)
+
+
+class QuickGELU(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
+
+class ResidualAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+
+        self.attn = nn.MultiheadAttention(d_model, n_head)
+        self.ln_1 = LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(d_model * 4, d_model))
+        ]))
+        self.ln_2 = LayerNorm(d_model)
+        self.attn_mask = attn_mask
+
+    def attention(self, x: torch.Tensor, padding_mask: torch.Tensor):
+        padding_mask = padding_mask.to(dtype=bool, device=x.device) if padding_mask is not None else None
+        self.attn_mask = self.attn_mask.to(device=x.device) if self.attn_mask is not None else None
+        return self.attn(x, x, x, need_weights=False, key_padding_mask=padding_mask, attn_mask=self.attn_mask)[0]
+
+    def forward(self, x):
+        x, padding_mask = x
+        x = x + self.attention(self.ln_1(x), padding_mask)
+        x = x + self.mlp(self.ln_2(x))
+        return (x, padding_mask)
+
+
+class Transformer(nn.Module):
+    def __init__(self, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+        self.width = width
+        self.layers = layers
+        self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers)])
+
+    def forward(self, x: torch.Tensor):
+        return self.resblocks(x)
+
+
+
+class VadInternVL(nn.Module):
+    def __init__(self,
+                 embed_dim: int,
+                 visual_length: int,
+                 visual_width: int,
+                 visual_head: int,
+                 visual_layers: int,
+                 attn_window: int,
+                 prompt_prefix: int,
+                 prompt_postfix: int,
+                 device):
+        super().__init__()

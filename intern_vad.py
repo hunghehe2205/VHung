25104e7195ff283de53973881da84c2aed6891c6
Nhat Minh <minhnhat211101@gmail.com>
2026-03-15 16:07:25 +0700

    update

--- intern_vad.py
+++ intern_vad.py
@@ -68,3 +68,112 @@ class VadInternVL(nn.Module):
                  prompt_postfix: int,
                  device):
         super().__init__()
+        self.visual_length = visual_length
+        self.visual_width = visual_width
+        self.embed_dim = embed_dim
+        self.attn_window = attn_window
+        self.prompt_prefix = prompt_prefix
+        self.prompt_postfix = prompt_postfix
+        self.device = device
+
+        self.temporal = Transformer(
+            width=visual_width,
+            layers=visual_layers,
+            heads=visual_head,
+            attn_mask=self.build_attention_mask(self.attn_window)
+        )
+
+        width = int(visual_width / 2)
+        self.gc1 = GraphConvolution(visual_width, width, residual=True)
+        self.gc2 = GraphConvolution(width, width, residual=True)
+        self.gc3 = GraphConvolution(visual_width, width, residual=True)
+        self.gc4 = GraphConvolution(width, width, residual=True)
+        self.disAdj = DistanceAdj()
+        self.linear = nn.Linear(visual_width, visual_width)
+        self.gelu = QuickGELU()
+
+        self.mlp1 = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(visual_width, visual_width * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(visual_width * 4, visual_width))
+        ]))
+        self.mlp2 = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(visual_width, visual_width * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(visual_width * 4, visual_width))
+        ]))
+        self.classifier = nn.Linear(visual_width, 1)
+
+        self.clipmodel, _ = clip.load("ViT-B/16", device)
+        for clip_param in self.clipmodel.parameters():
+            clip_param.requires_grad = False
+
+        self.frame_position_embeddings = nn.Embedding(visual_length, visual_width)
+        self.text_prompt_embeddings = nn.Embedding(77, self.embed_dim)
+
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        nn.init.normal_(self.text_prompt_embeddings.weight, std=0.01)
+        nn.init.normal_(self.frame_position_embeddings.weight, std=0.01)
+
+    def build_attention_mask(self, attn_window):
+        # lazily create causal attention mask, with full attention between the vision tokens
+        # pytorch uses additive attention mask; fill with -inf
+        mask = torch.empty(self.visual_length, self.visual_length)
+        mask.fill_(float('-inf'))
+        for i in range(int(self.visual_length / attn_window)):
+            if (i + 1) * attn_window < self.visual_length:
+                mask[i * attn_window: (i + 1) * attn_window, i * attn_window: (i + 1) * attn_window] = 0
+            else:
+                mask[i * attn_window: self.visual_length, i * attn_window: self.visual_length] = 0
+
+        return mask
+
+    def adj4(self, x, seq_len):
+        soft = nn.Softmax(1)
+        x2 = x.matmul(x.permute(0, 2, 1)) # B*T*T
+        x_norm = torch.norm(x, p=2, dim=2, keepdim=True)  # B*T*1
+        x_norm_x = x_norm.matmul(x_norm.permute(0, 2, 1))
+        x2 = x2/(x_norm_x+1e-20)
+        output = torch.zeros_like(x2)
+        if seq_len is None:
+            for i in range(x.shape[0]):
+                tmp = x2[i]
+                adj2 = tmp
+                adj2 = F.threshold(adj2, 0.7, 0)
+                adj2 = soft(adj2)
+                output[i] = adj2
+        else:
+            for i in range(len(seq_len)):
+                tmp = x2[i, :seq_len[i], :seq_len[i]]
+                adj2 = tmp
+                adj2 = F.threshold(adj2, 0.7, 0)
+                adj2 = soft(adj2)
+                output[i, :seq_len[i], :seq_len[i]] = adj2
+
+        return output
+
+    def encode_video(self, images, padding_mask, lengths):
+        images = images.to(torch.float)
+        position_ids = torch.arange(self.visual_length, device=self.device)
+        position_ids = position_ids.unsqueeze(0).expand(images.shape[0], -1)
+        frame_position_embeddings = self.frame_position_embeddings(position_ids)
+        frame_position_embeddings = frame_position_embeddings.permute(1, 0, 2)
+        images = images.permute(1, 0, 2) + frame_position_embeddings
+
+        x, _ = self.temporal((images, None))
+        x = x.permute(1, 0, 2)
+
+        adj = self.adj4(x, lengths)
+        disadj = self.disAdj(x.shape[0], x.shape[1])
+        x1_h = self.gelu(self.gc1(x, adj))
+        x2_h = self.gelu(self.gc3(x, disadj))
+
+        x1 = self.gelu(self.gc2(x1_h, adj))
+        x2 = self.gelu(self.gc4(x2_h, disadj))
+
+        x = torch.cat((x1, x2), 2)
+        x = self.linear(x)
+
+        return x
